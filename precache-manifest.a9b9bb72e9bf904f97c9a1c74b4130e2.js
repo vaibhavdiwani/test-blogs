6134fb6bb288c01f548f49fbb675d46b515a5205
Vaibhav Patel <vaibhav.patel@cimpress.com>
2019-12-28 17:50:45 +0530

    Updates

---
+++ precache-manifest.a9b9bb72e9bf904f97c9a1c74b4130e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb43bea072639d65519bad945b634f2c",
+    "url": "/test-blogs/index.html"
+  },
+  {
+    "revision": "cae7e8dc966d5d7f9bd2",
+    "url": "/test-blogs/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f923b87116e03626181a",
+    "url": "/test-blogs/static/css/main.210dba5b.chunk.css"
+  },
+  {
+    "revision": "cae7e8dc966d5d7f9bd2",
+    "url": "/test-blogs/static/js/2.37d3e901.chunk.js"
+  },
+  {
+    "revision": "6736e087bd6ad197168439612798b46d",
+    "url": "/test-blogs/static/js/2.37d3e901.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f923b87116e03626181a",
+    "url": "/test-blogs/static/js/main.0c005df2.chunk.js"
+  },
+  {
+    "revision": "9d3db05ed82bafc8441c",
+    "url": "/test-blogs/static/js/runtime-main.e4f5c1e4.js"
+  }
+]);
